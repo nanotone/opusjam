1fc52157a02112a2658f329be1af079371aef6c3
nanotone <nanotone@gmail.com>
2020-04-21 23:03:15 -0400

    Use non-blocking pyaudio

--- audio.py
+++ audio.py
@@ -1,10 +1,12 @@
 import logging
 import queue
+import threading
 import time
 
 import opuslib
 import pyaudio
 
+import stats
 import util
 
 
@@ -19,6 +21,10 @@ def get_audio():
 class Recorder:
     def __init__(self):
         self.enc = opuslib.Encoder(24000, 1, opuslib.APPLICATION_RESTRICTED_LOWDELAY)
+        self.enc.bitrate = 64000
+        self.enc.lsb_depth = 16
+        self.enc.packet_loss_perc = 25
+        self.enc.signal = opuslib.SIGNAL_MUSIC
         self.listeners = []
 
     def start(self):
@@ -46,57 +52,153 @@ class Recorder:
 
 
 class Player:
+    SILENCE = b'\0' * 240
+
     def __init__(self):
-        self.dec = opuslib.Decoder(24000, 1)
-        self.queue = queue.Queue(maxsize=5)
+        self.channels = {}
 
     def start(self):
-        self.playing = True
-        self.thread = util.start_daemon(self.run)
-
-    def stop(self, block=True):
-        self.playing = False
-        if block:
-            self.thread.join()
-        logging.info("Player thread stopped and joined")
-
-    def run(self):
-        stream = get_audio().open(
+        self.stream = get_audio().open(
             format=pyaudio.paInt16,
             channels=1,
             rate=24000,
             output=True,
             frames_per_buffer=120,
+            stream_callback=self.callback,
         )
-        last_log = 0
-        while self.playing:
-            try:
-                packet = self.queue.get_nowait()
-            except queue.Empty:
-                time.sleep(0.002)
-                continue
-            frame = self.dec.decode(packet, 120)
-            stream.write(frame)
-            now = time.time()
-            if now - last_log > 1:
-                if last_log:
-                    logging.info("played {} frames in {:.3f} sec; queue size {}".format(
-                        frames, now - last_log, self.queue.qsize()
-                    ))
-                frames = 0
-                last_log = now
-            frames += 1
-        stream.stop_stream()
-        stream.close()
 
-    def put_packet(self, data, addr):
-        data = data[4:]
+    def stop(self, block=True):
+        self.stream.stop_stream()
+        self.stream.close()
+
+    def put_packet(self, seq, data, peer_name):
+        channel = self.channels.get(peer_name)
+        if not channel:
+            # in lieu of a lock, use attribute assignment to synchronize
+            channels = dict(self.channels)
+            channels[peer_name] = channel = Channel()
+            self.channels = channels
+        channel.enqueue(seq, data)
+
+    def callback(self, in_data, frame_count, time_info, status):
+        channels = self.channels
+        for channel in channels.values():
+            data = channel.get_audio()
+            break
+        else:
+            data = Player.SILENCE
+        assert len(data) == 240
+        return (data, pyaudio.paContinue)
+
+
+class Channel:
+    __slots__ = (
+        'accept_rate',
+        'decoded',
+        'decoded_seq',
+        'decoder',
+        'decoder_lock',
+        'decoder_thread',
+        'dequeue_to',
+        'dupe_check',
+        'latest_seq',
+        'queue',
+        'wake_event',
+        'wake_lock',
+    )
+
+    def __init__(self):
+        self.accept_rate = 1.0
+        self.decoded = None
+        self.decoder = opuslib.Decoder(24000, 1)
+        self.decoder_lock = threading.Lock()
+        self.dequeue_to = -1
+        self.dupe_check = util.DupeCheck()
+        self.decoded_seq = -1
+        self.latest_seq = -1
+        self.queue = queue.PriorityQueue()
+        self.wake_event = threading.Event()
+        self.wake_lock = threading.Lock()
+        self.decoder_thread = util.start_daemon(self.run_decoder)
+
+    def enqueue(self, seq, data):
+        """Enqueue a packet with its sequence number, and wake the decoder."""
+        if not self.dupe_check.receive(seq):
+            return
+        #stats.METER('recv %', self.dupe_check.receive_rate * 100)
+        if seq > self.decoded_seq:
+            if self.decoded_seq == -1:
+                self.decoded_seq = seq - 1
+            self.accept_rate = self.accept_rate * 0.9 + 0.1
+            if seq > self.latest_seq:
+                self.latest_seq = seq
+                self.dequeue_to = seq - 3
+            stats.METER('seqrange', self.latest_seq - self.decoded_seq)
+            self.queue.put_nowait((seq, data))
+            self.wake_event.set()
+        else:
+            self.accept_rate *= 0.9
+        stats.METER('accept', self.accept_rate)
+
+    def dequeue(self):
         while True:
             try:
-                self.queue.put_nowait(data)
-                return
-            except queue.Full:
-                try:
-                    self.queue.get_nowait()
-                except queue.Empty:
-                    pass
+                (seq, packet) = self.queue.get_nowait()
+            except queue.Empty:
+                return (None, None)
+            if seq > self.decoded_seq:
+                break
+        if seq > self.dequeue_to:
+            # call it an underflow to preserve qsize (but requeue for later)
+            self.queue.put_nowait((seq, packet))
+            self.dequeue_to += 1
+            return (None, None)
+        self.decoded_seq = seq
+        if self.dequeue_to - seq > 2 and not self.dupe_check.saw(seq + 1):
+            self.decoded_seq = seq + 1
+        self.dequeue_to += 1
+        return (seq, packet)
+
+    def run_decoder(self):
+        while True:
+            self.wake_event.wait()
+            self.wake_event.clear()
+            if self.decoded:
+                continue
+            seq, packet = self.dequeue()
+            if not packet:
+                continue  # out of luck! sleep until more data comes
+            self.decoder_lock.acquire()
+            data = self.decoder.decode(packet, 120)
+            self.wake_lock.acquire()
+            self.decoded = data
+            self.decoder_lock.release()
+            self.wake_event.clear()
+            self.wake_lock.release()
+
+    def read_decoded(self):
+        """Return whatever is in the decoded buffer (possibly None) and wake
+        the decoder thread to let it know the buffer is empty."""
+        data = self.decoded
+        with self.wake_lock:
+            self.decoded = None
+            self.wake_event.set()
+        return data
+
+    def get_audio(self):
+        """Return a valid chunk of usable audio, regardless of whether the
+        decoder has real packets queued up."""
+        data = self.read_decoded()  # wakes decoder
+        if data:
+            stats.COUNT('present')
+            return data
+        # Prepare to decode a dropped frame, so acquire the lock first.
+        self.decoder_lock.acquire()
+        # If decoder was busy, it should already have provided fresh audio.
+        if self.decoded:
+            self.decoder_lock.release()
+            return self.read_decoded()  # wakes decoder
+        data = self.decoder.decode(b'', 120)
+        self.decoder_lock.release()
+        stats.COUNT('missing')
+        return data
